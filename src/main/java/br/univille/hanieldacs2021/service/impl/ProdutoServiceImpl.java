26421a4ad705ad424a3f98dacf306891031d2d27
Haniel Lourenço Lohn <hanielohn2016@gmail.com>
2021-10-04 21:01:20 +0000

    atrasado

--- src/main/java/br/univille/hanieldacs2021/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/univille/hanieldacs2021/service/impl/ProdutoServiceImpl.java
@@ -5,9 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.univille.hanieldacs2021.model.Fornecedor;
 import br.univille.hanieldacs2021.model.Produto;
 import br.univille.hanieldacs2021.repository.ProdutoRepository;
 import br.univille.hanieldacs2021.service.ProdutoService;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
 @Service
 public class ProdutoServiceImpl implements ProdutoService {
@@ -28,6 +40,40 @@ public class ProdutoServiceImpl implements ProdutoService {
     @Override
     public void delete(Produto produto) {
         repository.delete(produto);
+    }
+
+    @Override
+    public List<Produto> importProduto(Fornecedor fornecedor) {
+        if (fornecedor != null) {
+            try {
+                URL endereco = new URL(fornecedor.getUrlAPI());
+                HttpURLConnection conn = (HttpURLConnection) endereco.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+                int responseCode = conn.getResponseCode();
+
+                if (responseCode == 200) {
+                    Scanner leitor = new Scanner(endereco.openStream());
+
+                    StringBuilder jsonText = new StringBuilder();
+                    while (leitor.hasNext()) {
+                        jsonText.append(leitor.nextLine());
+                    }
+
+                    Gson gson = new Gson();
+
+                    Type typeListProdutos = new TypeToken<ArrayList<Produto>>() {
+                    }.getType();
+                    ArrayList<Produto> listaProdutos = gson.fromJson(jsonText.toString(), typeListProdutos);
+                    return listaProdutos;
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }

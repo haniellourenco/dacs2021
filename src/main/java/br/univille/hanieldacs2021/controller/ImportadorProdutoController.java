bfefa39377ab0a981bad1c094ea9a037a0b37f54
Haniel Lourenço Lohn <hanielohn2016@gmail.com>
2021-10-04 21:25:45 +0000

    fix atrasos

--- src/main/java/br/univille/hanieldacs2021/controller/ImportadorProdutoController.java
+++ src/main/java/br/univille/hanieldacs2021/controller/ImportadorProdutoController.java
@@ -6,55 +6,41 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.hanieldacs2021.model.Fornecedor;
 import br.univille.hanieldacs2021.model.Produto;
+import br.univille.hanieldacs2021.service.FornecedorService;
 
 @Controller
 @RequestMapping("/import-produto")
 public class ImportadorProdutoController {
 
+    @Autowired
+    private FornecedorService fornecedorService;
+
     @GetMapping
-    public ModelAndView index() {
+    public ModelAndView index(@ModelAttribute Fornecedor fornecedor) {
+        List<Fornecedor> listaFornecedor = fornecedorService.getAllFornecedores();
 
-        try {
-            URL endereco = new URL("http://1b22-186-237-248-5.ngrok.io/api/v1/produtos");
-            HttpURLConnection conn = (HttpURLConnection) endereco.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            int responseCode = conn.getResponseCode();
-            System.out.println(responseCode);
-            Scanner leitor = new Scanner(endereco.openStream());
+        return new ModelAndView("/importproduto/index", "listafornecedor", listaFornecedor);
+    }
 
-            StringBuilder jsonText = new StringBuilder();
-            while (leitor.hasNext()) {
-                jsonText.append(leitor.nextLine());
-            }
-
-            Gson gson = new Gson();
-
-            Type typeListProdutos = new TypeToken<ArrayList<Produto>>() {
-            }.getType();
-            ArrayList<Produto> listaProdutos = gson.fromJson(jsonText.toString(), typeListProdutos);
-
-            for (Produto umProduto : listaProdutos) {
-                System.out.println(umProduto.getDescricao());
-            }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    @PostMapping
+    public ModelAndView busca(Fornecedor fornecedor) {
+        System.out.println(fornecedor.getId());
         return new ModelAndView("/importproduto/index");
     }
 }
